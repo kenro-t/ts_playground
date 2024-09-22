a503dfde2ed96fd1e1f9685a022f179ff3e35766
kenro-t <139695337+kenro-t@users.noreply.github.com>
2024-09-22 10:54:41 +0000

    react学習

---
+++ react-introduction/react-guide-material/04_react_basic/src/070_component_nest/start/components/Child.jsx
@@ -0,0 +1,11 @@
+import { List } from "./List"
+import "./Child.css"
+
+export const Child = () => {
+    return (
+      <div className="component">
+        <h3>Child Component</h3>
+        <List />
+    </div>
+    );
+};
