6ae4889df76e83d119cbda16f00cb61ef3da77b9
kenro-t <139695337+kenro-t@users.noreply.github.com>
2024-09-23 12:47:43 +0000

    react学習

--- react-introduction/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
+++ react-introduction/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,17 @@
+import { Fragment } from "react";
 import "./Child.css";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <Fragment key='1'>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. In molestias, nobis quia error fugiat libero similique eum nam, alias voluptates quod minima dolor recusandae numquam hic doloremque aspernatur laboriosam. Accusamus.
+      </p>
+    </Fragment>
   );
 };
 
