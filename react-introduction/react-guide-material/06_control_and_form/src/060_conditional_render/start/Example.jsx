05aa7b63e8efd0cc57625918bf87c07d08b23439
kenro-t <139695337+kenro-t@users.noreply.github.com>
2024-09-29 14:20:52 +0000

    react学習

--- react-introduction/react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ react-introduction/react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,12 +15,15 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
             return isMatch;
           })
           .map((animal) => (
-            <li key={animal}>{animal}</li>
+            <li key={animal}>
+            {animal ?? "null,undefinedでした"}
+            {animal === "Dog" && "★"}
+              </li>
           ))}
       </ul>
     </>
@@ -28,3 +31,9 @@ const Example = () => {
 };
 
 export default Example;
+
+// Null 合体演算子 (??)
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+
+// 論理積 (&&)
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_AND
