a7937aacaf6dd616b3ea259fc8eba89878d0fd39
kenro-t <139695337+kenro-t@users.noreply.github.com>
2024-10-04 13:19:05 +0000

    react学習

--- react-introduction/react-guide-material/06_control_and_form/src/130_reminder/start/components/Form.jsx
+++ react-introduction/react-guide-material/06_control_and_form/src/130_reminder/start/components/Form.jsx
@@ -5,6 +5,7 @@ const Form = ({ createTodo }) => {
     const [enteredTodo, setEnteredTodo] = useState("");
 
     const addTodo = e => {
+        e.preventDefault();
         const newTodo = {
             id: Math.floor(Math.random() * 1e5),
             content: enteredTodo
@@ -15,13 +16,15 @@ const Form = ({ createTodo }) => {
     }
 
     return (
-        <>
-        <input type="text"
-            value={enteredTodo}
-            onChange={(e => setEnteredTodo(e.target.value))}
-        />
-        <button onClick={addTodo}>追加</button>
-        </>
+        <div>
+            <form onSubmit={addTodo}>
+                <input type="text"
+                    value={enteredTodo}
+                    onChange={(e => setEnteredTodo(e.target.value))}
+                />
+            <button>追加</button>
+            </form>
+        </div>
     );
 }
 
