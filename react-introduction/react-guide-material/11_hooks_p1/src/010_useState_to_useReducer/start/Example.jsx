f07d72648cfa7e3bb83e62d7823f0dab21a9ae1d
kenro-t <139695337+kenro-t@users.noreply.github.com>
2024-10-06 14:04:04 +0000

    react学習

--- react-introduction/react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ react-introduction/react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,9 +1,60 @@
+import { useState, useReducer } from "react";
+import { reducer } from "../../../../13_redux/end/src/020_actionCreator/store/modules/counter";
+
 const Example = () => {
+  const [val, setVal] = useState(0);
   
+  const countUp = () => {
+    setVal(prev => {
+      return ++prev;
+    })
+  }
+
+  // useStateの第一引数recucer関数にはsatateとactionが定義できる
+
+  // === 別関数として切り出した場合 ===
+  const reducer = (prev, {type}) => {
+    switch(type) {
+      case "+":
+        return ++prev;
+      case "-":
+        return --prev;
+    }
+  }
+
+  // reducerをセットするだけで引数が渡せる
+  const [rVal, dispatch] = useReducer(reducer, 0);
+
+  // ↓みたいに一度クロージャとして引数を取る必要がない
+  // const [rVal, dispatch] = useReducer((state, action) => reducer(state, action), 0)
+  // ================================
+
+  // 一つにまとめた場合
+  // const [rVal, dispatch] = useReducer((state, {type}) => {
+  //   switch(type) {
+  //     case "+":
+  //       return ++state;
+  //     case "-":
+  //       return --state;
+  //   }
+  // }, 0);  
+
+  const rCountUp = () => {
+    dispatch({type: "+"})
+  }
+  const rCountDown = () => {
+    dispatch({type: "-"})
+  }
   return (
     <>
+      <h3>{val}</h3>
+      <button onClick={countUp}>+</button>
+      <h3>{rVal}</h3>
+      <button onClick={rCountUp}>+</button>
+      <button onClick={rCountDown}>-</button>
     </>
   );
 };
 
 export default Example;
+
