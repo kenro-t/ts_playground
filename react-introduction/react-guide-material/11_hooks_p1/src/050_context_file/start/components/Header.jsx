f41c09c00e008a8741b97c02614bf410a8dc9931
kenro-t <139695337+kenro-t@users.noreply.github.com>
2024-10-09 13:18:39 +0000

    react途中まで

--- react-introduction/react-guide-material/11_hooks_p1/src/050_context_file/start/components/Header.jsx
+++ react-introduction/react-guide-material/11_hooks_p1/src/050_context_file/start/components/Header.jsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import { ThemeContext } from "../Example";
+
 const Header = () => {
-  
+    const [theme, setTheme] = useContext(ThemeContext);
+    const THEMES = ['light', 'dark', 'red'];
+    const changeTheme = (e) => setTheme(e.target.value)
+
+    return (
+        <header className={`content-${theme}`}>
+            {THEMES.map(_theme => (
+            <label key={_theme}>
+                <input type="radio"
+                value={_theme}
+                onChange={changeTheme}
+                checked={_theme === theme}
+                />
+                {_theme}
+            </label>
+            ))}
+        </header>
+    );
 };
 
 export default Header;
