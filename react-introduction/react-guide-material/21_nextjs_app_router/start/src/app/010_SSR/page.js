d9ec7aa872bdcc14a2ffe88cc653c10a1d90850d
kenro-t <ngkn1425@gmail.com>
2024-10-21 14:21:33 +0000

    next.js学習

--- react-introduction/react-guide-material/21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ react-introduction/react-guide-material/21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,5 +1,20 @@
+"use client"
+// clientで利用するものは上記のように明示する必要あり
+
+import { useEffect, useState } from "react"
+
 export default function SSR() {
- return <div>SSR Page</div>   
+    const [state, setState] = useState(undefined);
+    // clientで利用するものはuseEffectで宣言する
+    useEffect(() => {
+        setState('client loaded');
+    },[])
+    return (
+        <>
+            <div>SSR Page</div>
+            <div>{state}</div>
+        </>
+    )   
 }
 
 // appディレクトリのディレクトリでルーティングできる
